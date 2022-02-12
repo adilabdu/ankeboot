dd408bd7ed021b46ddf38b28edd0000444bf15cf
adil <adilabdu68@gmail.com>
2022-02-13 00:47:08 +0900

    Landing page changed to coming soon page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <script setup>
     import Home from "./components/Home.vue"
     import Landing from "./components/Landing.vue"
+    import ComingSoon from "./views/coming-soon.vue"
 </script>
 
 <template>
-    <Landing/>
-    <!-- <Home/> -->
+    <!-- <Landing/> -->
+    <coming-soon/>
 </template>
 
-<style></style>
+<style>
+
+::-webkit-scrollbar {
+    width: 7px;
+    display: hidden; 
+}
+
+::-webkit-scrollbar-track {
+    background: #fff; 
+}
+
+::-webkit-scrollbar-thumb {
+    /* background: rgba(0,0,0,0.5); */
+    background: rgb(255, 255, 255); 
+    border-radius: 30px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background: #555; 
+}
+
+::-webkit-scrollbar:hover {
+    width: 100px;
+}
+
+</style>
