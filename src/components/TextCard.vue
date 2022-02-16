17c7e862b6d413ca9bbb6c0cb3025779061d8502
adil <adilabdu68@gmail.com>
2022-02-16 17:31:14 +0900

    New TextCard component encapsulated, for text displays

---
+++ src/components/TextCard.vue
@@ -0,0 +1,26 @@
+<script>
+
+    export default {
+
+        name: "TextCard",
+        components: {
+
+        },
+
+        setup() {
+            
+        }
+
+    }
+
+</script>
+
+<template>
+    
+    <p class="font-sans font-medium p-8 bg-gradient-to-r from-publishing-50 to-maps-50 rounded-lg drop-shadow-md transition duration-300 hover:shadow-xl hover:scale-105">
+        <slot/>
+    </p>
+
+</template>
+
+<style scoped></style>
