e79b3eeb2ec771d898febe49ed684c7eb0851814
adilabdu <adilabdu68@gmail.com>
2022-03-27 12:12:44 +0300

    Cleaning up commented lines

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,17 @@
 <script setup>
-    import Home from "./components/Home.vue"
-    import Landing from "./components/Landing.vue"
-    import ComingSoon from "./views/coming-soon.vue"
     import First from "./components/First.vue"
 </script>
 
-<script>
-
-// export default {
-//   // ...
-//   methods: {
-//     setViewHeight: function() {
-//       let vh = window.innerHeight * 0.01
-//       document.documentElement.style.setProperty('--vh', `${vh}px`)
-//     },
-//   },
-//   mounted: function() {
-//     this.setViewHeight()
-//     window.addEventListener('resize', () => {
-//       this.setViewHeight()
-//     })
-//   },
-//   // ...
-// }
-
-</script>
-
 <template>
-<!--    <Landing/>-->
+
     <First/>
-    <!-- <coming-soon/> -->
+
 </template>
 
 <style>
 
 ::-webkit-scrollbar {
     width: 7px;
-    display: hidden; 
 }
 
 ::-webkit-scrollbar-track {
