7afe7ba19da720b651d69d3edcb4632aa5634d98
adilabdu <adilabdu68@gmail.com>
2022-04-14 22:01:19 +0900

    Amharic and English texts

---
+++ src/assets/text/first.js
@@ -0,0 +1,12 @@
+export const text = {
+    "en": {
+        title: "Ankeboot",
+        subtitle: "",
+        hero_tag: "inspired by the creativity and perseverance of a __spider__"
+    },
+
+    "am": {
+        title: "አንከቡት",
+        hero_tag: "በሸረሪት ፈጠራ እና ጽናት ተመስጦ"
+    }
+}
